79482ad3aa64263dcd38048dc452632c4cd1d584
devalexedmondson <alexedmondson20@gmail.com>
2017-02-01 14:49:55 -0600

    added Unit test project

---
+++ CustomListClassProject/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CustomListClassProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CustomListClassProject
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
